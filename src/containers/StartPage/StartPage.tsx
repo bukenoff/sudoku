9aec58906e82fb59136f676519aee047a572467c
serik <bukenoff.s@gmail.com>
2021-01-03 19:13:39 +0700

    added link to game from home page

--- src/containers/StartPage/StartPage.tsx
+++ src/containers/StartPage/StartPage.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 export const StartPage: FC = () => {
-  return <section>start page</section>;
+  return (
+    <section>
+      <Link to="/game">start game</Link>
+    </section>
+  );
 };
