74e85b8d3135cf2f660c13536ce35e5283d599f6
serik <bukenoff.s@gmail.com>
2021-01-08 18:51:01 +0700

    fixed cell tests, added test case when value is not guessed right

--- src/components/grid/Cell/Cell.test.tsx
+++ src/components/grid/Cell/Cell.test.tsx
@@ -4,27 +4,57 @@ import { Cell, ICellProps } from './Cell';
 
 describe('Cell component:', () => {
   it('should render without crash', () => {
-    const restProps = {} as ICellProps;
+    const setGuessedValueMock = jest.fn() as ICellProps['setGuessedValue'];
+    const clearGuessedValueMock = jest.fn() as ICellProps['clearGuessedValue'];
+
     const { getByTestId } = render(
-      <Cell {...restProps} is_resolved={true} value={5} />,
+      <Cell
+        is_resolved={true}
+        value={5}
+        guessed_value={5}
+        block_index={1}
+        cell_index={2}
+        setGuessedValue={setGuessedValueMock}
+        clearGuessedValue={clearGuessedValueMock}
+      />,
     );
     const cell_component = getByTestId('cell_root');
     expect(cell_component).toBeInTheDocument();
   });
 
   it('should not render value', () => {
-    const restProps = {} as ICellProps;
+    const setGuessedValueMock = jest.fn() as ICellProps['setGuessedValue'];
+    const clearGuessedValueMock = jest.fn() as ICellProps['clearGuessedValue'];
+
     const { getByTestId } = render(
-      <Cell {...restProps} is_resolved={false} value={5} />,
+      <Cell
+        is_resolved={false}
+        value={5}
+        guessed_value={0}
+        block_index={1}
+        cell_index={2}
+        setGuessedValue={setGuessedValueMock}
+        clearGuessedValue={clearGuessedValueMock}
+      />,
     );
     const cell_value = getByTestId('cell_value');
     expect(cell_value.textContent).toEqual(' ');
   });
 
   it('should render digits selection', () => {
-    const restProps = {} as ICellProps;
+    const setGuessedValueMock = jest.fn() as ICellProps['setGuessedValue'];
+    const clearGuessedValueMock = jest.fn() as ICellProps['clearGuessedValue'];
+
     const { getByTestId } = render(
-      <Cell {...restProps} is_resolved={false} value={5} />,
+      <Cell
+        is_resolved={false}
+        value={5}
+        guessed_value={0}
+        block_index={1}
+        cell_index={2}
+        setGuessedValue={setGuessedValueMock}
+        clearGuessedValue={clearGuessedValueMock}
+      />,
     );
     const cell_component = getByTestId('cell_root');
 
@@ -33,5 +63,25 @@ describe('Cell component:', () => {
     const digits_selection_root = getByTestId('digits_selection_root');
 
     expect(digits_selection_root).toBeInTheDocument();
+  });
+
+  it('should highlight value red when it is not equal to guessed value', () => {
+    const setGuessedValueMock = jest.fn() as ICellProps['setGuessedValue'];
+    const clearGuessedValueMock = jest.fn() as ICellProps['clearGuessedValue'];
+
+    const { getByTestId } = render(
+      <Cell
+        is_resolved={false}
+        value={5}
+        guessed_value={0}
+        block_index={1}
+        cell_index={2}
+        setGuessedValue={setGuessedValueMock}
+        clearGuessedValue={clearGuessedValueMock}
+      />,
+    );
+
+    const cell_value = getByTestId('cell_value');
+    expect(cell_value).toHaveStyle('color: red');
   });
 });
