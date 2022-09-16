de455812550275598e76b07fb3f313066dfead59
serik <bukenoff.s@gmail.com>
2022-09-16 19:46:45 +0700

    Add redirect on victory

--- src/containers/GamePage/GamePage.tsx
+++ src/containers/GamePage/GamePage.tsx
@@ -4,19 +4,26 @@ import Grid from '~/components/grid/Grid';
 import { useStores } from '~/stores/stores.provider';
 import * as Styled from './styles';
 import PauseOverlay from '~/components/overlays/PauseOverlay';
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps, useHistory } from 'react-router';
+import { Difficulty } from '~/models';
 
-export const GamePage: FC<
-  RouteComponentProps<{ difficulty: 'easy' | 'medium' | 'hard' }>
-> = observer(({ match }) => {
+type GamePageProps = RouteComponentProps<{ difficulty: Difficulty }>;
+
+export const GamePage: FC<GamePageProps> = observer(({ match }) => {
   const {
-    grid_store: { fetchGrid, grid, is_fetching },
+    grid_store: { fetchGrid, grid, is_fetching, is_resolved },
     timer_store: { is_paused },
   } = useStores();
+
+  const history = useHistory();
 
   useEffect(() => {
     fetchGrid(match.params.difficulty);
   }, []);
+
+  useEffect(() => {
+    is_resolved && history.push('/scores');
+  }, [is_resolved]);
 
   return (
     <Styled.Root>
