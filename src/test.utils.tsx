4fc42edc94510331486cd633536d7c41961361f6
serik <bukenoff.s@gmail.com>
2023-04-20 21:56:15 +0700

    :sparkles: Wrap component tests in vitest

---
+++ src/test.utils.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/export */
+import { render } from '@testing-library/react';
+
+function customRender(ui: React.ReactElement, options = {}) {
+  return render(ui, {
+    // wrap provider(s) here if needed
+    // @ts-ignore
+    wrapper: ({ children }) => children,
+    ...options,
+  });
+}
+
+export * from '@testing-library/react';
+export { default as userEvent } from '@testing-library/user-event';
+// override render export
+export { customRender as render };
