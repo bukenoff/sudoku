e91eeb67123911ec330c6551ada265aadf5702d0
serik <bukenoff.s@gmail.com>
2023-04-20 21:16:43 +0700

    ➕ Install chunk split plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
       '~': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), chunkSplitPlugin()],
 });
