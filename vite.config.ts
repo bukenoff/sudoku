1a5671b3d1ad268ecf4d3866f696352c0c6b86a4
serik <bukenoff.s@gmail.com>
2023-04-21 20:00:41 +0700

    :sparkles: Split vendor

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import path from 'path';
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+import { dependencies } from './package.json';
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
@@ -15,5 +26,13 @@ export default defineConfig({
   plugins: [react(), splitVendorChunkPlugin()],
   build: {
     minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
   },
 });
