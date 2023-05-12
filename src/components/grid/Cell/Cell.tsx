c1acaaf0e1ad166a5675eab36cdb740b88d2f7c3
serik <bukenoff.s@gmail.com>
2023-05-12 20:41:32 +0700

    ⚡️ Memoize cell

--- src/components/grid/Cell/Cell.tsx
+++ src/components/grid/Cell/Cell.tsx
@@ -1,4 +1,4 @@
-import React, { FC, KeyboardEventHandler, useState } from 'react';
+import React, { FC, KeyboardEventHandler, memo, useState } from 'react';
 import ClickAwayListener from 'react-click-away-listener';
 
 import { useStores } from '~/stores';
@@ -15,55 +15,52 @@ export interface CellProps {
   cell_index: ICell['cell_index'];
 }
 
-export const Cell: FC<CellProps> = ({
-  value,
-  guessed_value,
-  block_index,
-  cell_index,
-  is_resolved,
-}) => {
-  const {
-    grid_store: { setGuessedValue, clearGuessedValue },
-  } = useStores();
-  const [is_selected, setSelected] = useState(false);
-  const display_digits_selection = is_resolved === false && is_selected;
+export const Cell: FC<CellProps> = memo(
+  ({ value, guessed_value, block_index, cell_index, is_resolved }) => {
+    const {
+      grid_store: { setGuessedValue, clearGuessedValue },
+    } = useStores();
+    const [is_selected, setSelected] = useState(false);
+    const display_digits_selection = is_resolved === false && is_selected;
 
-  const onCellClick = () => setSelected(!is_selected);
-  const onClickAway = () => setSelected(false);
-  console.log('render');
-  const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (/[1-9]/g.test(e.key)) {
-      setGuessedValue(block_index, cell_index, +e.key as any); // TODO: Fix any
-      setSelected(false);
-    }
-  };
+    const onCellClick = () => setSelected(!is_selected);
+    const onClickAway = () => setSelected(false);
+    const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+      if (/[1-9]/g.test(e.key)) {
+        setGuessedValue(block_index, cell_index, +e.key as any); // TODO: Fix any
+        setSelected(false);
+      }
+    };
 
-  return (
-    <ClickAwayListener onClickAway={onClickAway} data-testid="cell_root">
-      <Styled.Root
-        is_resolved={is_resolved}
-        value={value}
-        guessed_value={guessed_value}
-        is_selected={is_selected}
-      >
-        <span data-testid="cell_value">
-          <input
-            onClick={onCellClick}
-            disabled={is_resolved}
-            value={is_resolved ? value : guessed_value || ' '}
-            onKeyDown={onKeyDown}
-            readOnly
-          />
-        </span>
-        {display_digits_selection && (
-          <DigitsSelection
-            block_index={block_index}
-            cell_index={cell_index}
-            setGuessedValue={setGuessedValue}
-            clearGuessedValue={clearGuessedValue}
-          />
-        )}
-      </Styled.Root>
-    </ClickAwayListener>
-  );
-};
+    return (
+      <ClickAwayListener onClickAway={onClickAway} data-testid="cell_root">
+        <Styled.Root
+          is_resolved={is_resolved}
+          value={value}
+          guessed_value={guessed_value}
+          is_selected={is_selected}
+        >
+          <span data-testid="cell_value">
+            <input
+              onClick={onCellClick}
+              disabled={is_resolved}
+              value={is_resolved ? value : guessed_value || ' '}
+              onKeyDown={onKeyDown}
+              readOnly
+            />
+          </span>
+          {display_digits_selection && (
+            <DigitsSelection
+              block_index={block_index}
+              cell_index={cell_index}
+              setGuessedValue={setGuessedValue}
+              clearGuessedValue={clearGuessedValue}
+            />
+          )}
+        </Styled.Root>
+      </ClickAwayListener>
+    );
+  },
+);
+
+Cell.displayName = 'Cell';
